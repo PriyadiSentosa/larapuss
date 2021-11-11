e966ba25e6c93a83cbe3c64cb0ac865b83b0ac05
PriyadiSentosa <priyadisentosa825@gmail.com>
2021-11-11 16:32:52 +0700

    Crud

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -132,15 +132,13 @@
         </a>
       </li>
       <li class="nav-item">
-        <a id="nav-link" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }}
-        </a>
 
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="{{ route('logout') }}"
+
+        <div class="nav-link" aria-labelledby="nav-link">
+            <a class="nav-link" href="{{ route('logout') }}"
                onclick="event.preventDefault();
                              document.getElementById('logout-form').submit();">
-                {{ __('Logout') }}
+                {{ __('Logout') }} <i class="fas fa-sign-out-alt"></i>
             </a>
 
             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
