1116b91972de78389243b36bae578c7a13552f70
PriyadiSentosa <priyadisentosa825@gmail.com>
2021-11-12 14:59:39 +0700

    Crud Buku

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+
     protected $fillable = ['title','author_id','amount','cover'];
     protected $visible = ['title','author_id','amount','cover'];
     public $timestamps= true;
@@ -18,6 +20,23 @@ class Book extends Model
         // data Model 'Book' bisa dimiliki oleh Model "Author"
         // melalui fk "author_id"
         return $this->belongsTo('App\Models\Book', 'author_id');
+    }
+
+    public function image()
+    {
+        if ($this->cover && file_exists(public_path('images/books/' . $this->cover))) {
+            return asset('images/books/' . $this->cover);
+        } else {
+            return asset('images/no_image.png');
+        }
+    }
+
+    public function deleteImage()
+    {
+        if ($this->cover && file_exists(public_path('images/books/' . $this->cover))) {
+            return unlink(public_path('images/books/' . $this->cover));
+        }
+
     }
 }
 
